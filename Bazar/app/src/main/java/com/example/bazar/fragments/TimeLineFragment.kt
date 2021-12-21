ddca9bf21f89cfc96a5709f94ec10399614b2347
D-SZAdorjan <demenyador@gmail.com>
2021-12-21 16:40:21 +0200

    mukodo fejléc
    + toolbar

--- Bazar/app/src/main/java/com/example/bazar/fragments/TimeLineFragment.kt
+++ Bazar/app/src/main/java/com/example/bazar/fragments/TimeLineFragment.kt
@@ -1,17 +1,22 @@
 package com.example.bazar.fragments
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
+import android.widget.ImageView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bazar.App
 import com.example.bazar.DataAdapter
+import com.example.bazar.R
 import com.example.bazar.databinding.FragmentTimeLineBinding
+import com.example.bazar.databinding.TopToolbarBinding
 import com.example.bazar.model.Product
 import com.example.bazar.viewmodels.MainScreenViewModel
 
@@ -24,6 +29,12 @@ class TimeLineFragment : Fragment(), DataAdapter.OnItemClickListener, DataAdapte
     private val mainScreenViewModel : MainScreenViewModel by activityViewModels()
     private lateinit var recView: RecyclerView
     private lateinit var adapter: DataAdapter
+
+    private lateinit var toolBar: View
+    private lateinit var bazaarLogo: ImageView
+    private lateinit var searchBtn: ImageView
+    private lateinit var filterBtn: ImageView
+    private lateinit var avatar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +49,8 @@ class TimeLineFragment : Fragment(), DataAdapter.OnItemClickListener, DataAdapte
         _binding = FragmentTimeLineBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        setUpToolBar()
+
         recView = binding.recyclerView
         setupRecyclerView()
         mainScreenViewModel.getProducts()
@@ -46,6 +59,20 @@ class TimeLineFragment : Fragment(), DataAdapter.OnItemClickListener, DataAdapte
             adapter.notifyDataSetChanged()
         }
         return view
+    }
+
+    private fun setUpToolBar(){
+        toolBar = requireActivity().findViewById(R.id.mainScreen_topToolBar)
+        bazaarLogo = requireActivity().findViewById(R.id.mainScreen_bazaar_logo)
+        searchBtn = requireActivity().findViewById(R.id.mainScreen_searchBtn)
+        filterBtn = requireActivity().findViewById(R.id.mainScreen_filterBtn)
+        avatar = requireActivity().findViewById(R.id.mainScreen_defaultAvatar)
+
+        toolBar.visibility = View.VISIBLE
+        bazaarLogo.visibility = View.VISIBLE
+        searchBtn.visibility = View.VISIBLE
+        filterBtn.visibility = View.VISIBLE
+        avatar.visibility = View.VISIBLE
     }
 
     private fun setupRecyclerView(){
